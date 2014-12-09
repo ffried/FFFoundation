ca915e59337e4d6b470166537a759fab5f5abbdd
Florian Friedrich <ffried@me.com>
2014-12-09 18:03:10 +0100

    Initial Commit

---
+++ FFFoundation/FFFoundation/FFFoundation.h
@@ -0,0 +1,19 @@
+//
+//  FFFoundation.h
+//  FFFoundation
+//
+//  Created by Florian Friedrich on 9.12.14.
+//  Copyright (c) 2014 Florian Friedrich. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for FFFoundation.
+FOUNDATION_EXPORT double FFFoundationVersionNumber;
+
+//! Project version string for FFFoundation.
+FOUNDATION_EXPORT const unsigned char FFFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFFoundation/PublicHeader.h>
+
+
---
+++ FFFoundation/FFFoundation/FunctionsAndHelpers.swift
@@ -0,0 +1,24 @@
+//
+//  FunctionsAndHelpers.swift
+//  FFFoundation
+//
+//  Created by Florian Friedrich on 9.12.14.
+//  Copyright (c) 2014 Florian Friedrich. All rights reserved.
+//
+
+import Foundation
+
+internal class FunctionsAndHelpers {}
+
+public func delay(delay: Double, block: ()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), block)
+}
+
+public func localizedString(key: String, comment: String = "") -> String {
+    return NSLocalizedString(key, comment: comment)
+}

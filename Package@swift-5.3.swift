d4325e1ffdaca4500a6c07d428741d1b34c5684c
Florian Friedrich <ffried@me.com>
2021-09-21 07:31:30 +0200

    Raise deployment target to iOS 11, macOS 10.13 and watchOS 4

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "FFFoundation",
     platforms: [
-       .macOS(.v10_12),
-       .iOS(.v10),
-       .tvOS(.v10),
-       .watchOS(.v3),
+       .macOS(.v10_13),
+       .iOS(.v11),
+       .tvOS(.v11),
+       .watchOS(.v4),
     ],
     products: [
       // Products define the executables and libraries produced by a package, and make them visible to other packages.
